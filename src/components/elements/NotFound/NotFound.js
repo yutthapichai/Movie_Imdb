2decc2d01a039a08ad038beb74ef4649b04744ff
Yutthapichai <cancel_go@hotmail.com>
2018-09-13 21:06:24 +0700

    Create Router by import Browser,Route,Switch

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>
+                Whooooop. this page doesn't exist
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound
